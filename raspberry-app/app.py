07a9519e5dea71bd45d8eabb31d18a5ebdde4cdb
Julien Martins Da Costa <julien.martinsdacosta@gmail.com>
2016-05-06 20:22:50 +0200

    Ajout de nouveaux événements socket.io pour le lancement de l'objet

--- raspberry-app/app.py
+++ raspberry-app/app.py
@@ -5,6 +5,7 @@ import re
 import serial
 import sys
 import time
+import uuid
 
 import accelerometer as ACCELEROMETER
 import button as BUTTON
@@ -20,6 +21,8 @@ this = sys.modules[__name__]
 # définir le port serial (1: raspberry / 2: mac OS X)
 #this.ser = serial.Serial(port="/dev/ttyACM0", baudrate=9600)
 this.ser = serial.Serial(port="/dev/cu.usbmodem1411", baudrate=9600)
+
+# attendre quelques secondes avant le lancement de l'objet
 time.sleep(3)
 
 
@@ -36,15 +39,15 @@ def printEvent(component, value):
 
 # Lors de la réception d'un évenement, on execute
 # la fonction qui est associée à celui-ci
-def executeEvent(component, value, socketIO):
+def executeEvent(component, value):
     if (component == "ACCELEROMETER"):
-        ACCELEROMETER.listener(value, socketIO)
+        ACCELEROMETER.listener(value, this.socketIO)
     if (component == "BUTTON"):
-        BUTTON.listener(value, socketIO)
+        BUTTON.listener(value, this.socketIO)
     if (component == "POTENTIOMETER"):
-        POTENTIOMETER.listener(value, socketIO)
+        POTENTIOMETER.listener(value, this.socketIO)
     if (component == "RFID"):
-        RFID.listener(value, socketIO)
+        RFID.listener(value, this.socketIO)
 
 
 # Lors de la réception d'un évenement, on le parse pour
@@ -55,29 +58,40 @@ def parseEvent(event):
 
 # Écouter tout les evenements de notre objet
 # qui sont envoyés à travers le raspberry
-def eventListener(socketIO):
+def eventListener():
+    print this.socketIO
     while 1:
         try:
             event = this.ser.readline()
             datas = parseEvent(event)
             if (datas):
                 printEvent(datas.group(1).strip(), datas.group(2).strip())
-                executeEvent(datas.group(1).strip(), datas.group(2).strip(), socketIO)
+                executeEvent(datas.group(1).strip(), datas.group(2).strip())
         except KeyboardInterrupt:
             print "\nPainperdu connected object \033[91m[STOPPED]\033[0m\n"
             break
 
 
-# Démarrer l'objet connecté
-def startPainperduConnectedObject(server, port):
+# Démarrer l'objet connecté si le numéro d'identifiant
+# est le même que celui envoyé par le serveur
+def runObject(objectId):
+    if (objectId == this.objectId):
+        eventListener()
+
+
+def main(server, port):
     print "\nPainperdu connected object \033[92m[STARTED]\033[0m\n"
 
+    # générer un id unique pour l'objet
+    this.objectId = str(uuid.uuid4())
+
     # connexion au serveur socket.io
-    socketIO = SocketIO(server, port)
-    socketIO.on("startPainperduConnectedObject", eventListener(socketIO))
-    socketIO.wait()
+    this.socketIO = SocketIO(server, port)
+    this.socketIO.emit("newObjectId", this.objectId)
+    this.socketIO.on("runObjectId", runObject)
+    this.socketIO.wait()
 
 
 # Éxecuter l'application
 if __name__ == '__main__':
-    startPainperduConnectedObject("localhost", 1337)
+    main("localhost", 1337)
